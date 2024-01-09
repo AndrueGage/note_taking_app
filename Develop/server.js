ed8dfbd9f6c92c8bb14fd0e13c342857dd77b0cc
AndrueGage <trainer.andrue@gmail.com>
2024-01-08 22:34:26 -0600

    added app.use

--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const notesRoute = require('./routes/notes');
+const homeRoute = require('./routes/home')
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
+app.use(notesRoute);
+app.use(homeRoute);
 app.use(express.static('public'));
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
